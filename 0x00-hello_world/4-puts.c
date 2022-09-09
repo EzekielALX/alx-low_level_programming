0fb8fa78632568d8272b58fba8579d19cb006825
Kehinde Ezekiel Ademola <kehindeeademola731@gmail.com>
2022-09-08 20:30:18 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
-Type in the following lines of code
-
-#include <stdio.h>
+include <stdio.h>
 /**
- *  *main- Serves as the entry point for the program
- *   *
- *    *Return: By default returns zero if no erros otherwise returns non-zero value.
- *     */
+ *  * main - main block
+ *   * Return: 0
+ *    */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
