85da96210348afde0f4905076b7cf2fe30534ac3
Kehinde Ezekiel Ademola <kehindeeademola731@gmail.com>
2022-09-14 15:30:53 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,10 @@
 #include "main.h"
+
 /**
- * main-main block
- * Description:A program that print _putchar
- * Return: 0 success
+ * main - Prints putchar
+ * @void: Empty parameter list for main.
+ *
+ * Description: Prints putchar w/out stdio
+ *
+ * Return: 0 for success
  */
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	
-	return (0)
-})
