5d39351b60648a3892e19bdfcc4ac0de649cc47b
Kehinde Ezekiel Ademola <kehindeeademola731@gmail.com>
2022-09-18 03:26:24 -0700

    all

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,22 +7,3 @@
  * Return :0
  */
 int main(void)
-
-	srand(time(0));
-	n - rand() - RAND_MAXX / 2;
-	/* your code goes here */
-	if (n > 0)
-{
-	print ("%1 is possitive\n", n);
-}
-else if (n == 0)
-{
-	printf("%1 is zero\n", n);
-}
-else if (n < 0)
-{
-	printf("%1 is negative\n", n);
-}
-return (0);
-}
-"cc
