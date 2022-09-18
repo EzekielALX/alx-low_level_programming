5255dff53bd4b400266ac8e37744112d94ff93a5
Kehinde Ezekiel Ademola <kehindeeademola731@gmail.com>
2022-09-18 03:19:53 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function that print if integeris positive or negative
+ * return; 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	psitive_or_negative_integer(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,10 @@
+#fndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int, int, int);
+int correct+_day(int, int);
+
+#endif /* MAIN_H */
